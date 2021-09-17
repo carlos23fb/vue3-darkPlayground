0c2f3412aa7e1f8e42cfaede6823c0af7fe84c5e
Carlos Becerra <carlos23fb@github.com>
2021-09-17 17:25:30 -0600

    Click Events

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
             title: 'Final Empire',
             author: 'Brandon Sanderson',
-            age:54
+            age: 54
         }
-    }
+    },
+    methods: {
+        changeTitle(bookTitle){
+            this.title = bookTitle
+        }
+    },
+    
     // data, functions
     // template: '<h2>I am the template</h2>'
 })

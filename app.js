f8c9e5257cdb81574b55e27f04a6722ebc834c22
Carlos Becerra <carlos23fb@github.com>
2021-09-17 16:24:19 -0600

    Data & Templates

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const app = Vue.createApp({
+    data(){
+        return {
+            title: 'Final Empire',
+            author: 'Brandon Sanderson',
+            age:54
+        }
+    }
     // data, functions
     // template: '<h2>I am the template</h2>'
 })
